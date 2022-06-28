9515cfba2db9409778a8bc69ab40d5ff7e66672a
LEMSYAH Mustapha <83125801+lemsyahmust@users.noreply.github.com>
2022-06-28 23:26:24 +0100

    Update views.py

--- views.py
+++ views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.views import APIView
 
 from ..models import *
 
